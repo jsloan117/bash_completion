32bbffb7a8e65b66ac97b5055f3e363e7b853609
Jonathan Sloan <jsloan117@gmail.com>
2018-07-28 16:41:11 -0500

    updated kitchen_completion, to reflect new version of kitchen and cmds.

--- kitchen_completion.bash
+++ kitchen_completion.bash
@@ -1,3 +1,7 @@
+#!/bin/bash
+# This is beter than the first, but not perfect.. when indoubt 'kitchen help' || https://kitchen.ci
+# Original -- https://github.com/MarkBorcherding/test-kitchen-bash-completion
+
 _kitchen_instance_list () {
   # cache to .kitchen.list.yml
   if [[ .kitchen.yml -nt .kitchen.list.yml || .kitchen.local.yml -nt .kitchen.list.yml ]]; then
@@ -7,22 +11,34 @@ _kitchen_instance_list () {
   cat .kitchen.list.yml
 }
 
+_kitchen_update_cmds () {
+  kitchen help | awk '{print $2}' | sed '/^$/d'
+}
+
 _kitchen_options () {
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
   COMPREPLY=()
 
   case $prev in
-    converge|create|destroy|diagnose|exec|list|login|package|setup|test|verify)
-      COMPREPLY=( $(compgen -W "$(_kitchen_instance_list)" -- ${cur} ))
+    converge|create|destroy|diagnose|doctor|exec|list|login|package|setup|test|verify)
+      COMPREPLY=($(compgen -W "$(_kitchen_instance_list)" -- "${cur}"))
       return 0
       ;;
-    driver)
-      COMPREPLY=( $(compgen -W "create discover help"  -- ${cur} ))
+    help)
+      COMPREPLY=($(compgen -W "$(_kitchen_update_cmds)" -- "${cur}"))
+      return 0
+      ;;
+    exec)
+      COMPREPLY=($(compgen -W "${_kitchen_instance_list}" -- "${cur}"))
+      return 0
+      ;;
+    *-*) # args to cmds -- not smartly filtered, so know which go to what cmds. (Was made for the above exec, but this is the only way I could get '-c' in atm)
+      COMPREPLY=($(compgen -W "-c -p -l -D -t -a -P --create-gemfile --no-create-gemfile -b -j -d" -- "${cur}"))
       return 0
       ;;
     *)
-      COMPREPLY=( $(compgen -W "console converge create destroy diagnose driver exec help init list login package setup test verify version"  -- ${cur} ))
+      COMPREPLY=($(compgen -W "$(_kitchen_update_cmds)" -- "${cur}"))
       return 0
       ;;
   esac
